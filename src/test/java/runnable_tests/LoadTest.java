1446c2ef194bf3089aef7293f245f48f185bd7b4
Carlos Castro <cpac20.4@gmail.com>
2020-07-21 20:57:39 +0100

    Add TestMulti

--- src/test/java/runnable_tests/LoadTest.java
+++ src/test/java/runnable_tests/LoadTest.java
@@ -1,17 +1,18 @@
 package runnable_tests;
 
 import io.atomix.utils.net.Address;
+import nosql.KeyValueDriver;
+import nosql.MongoAsynchKV;
 import transaction_manager.OperationFailedException;
 import transaction_manager.Transaction;
 import transaction_manager.TransactionController;
 import transaction_manager.TransactionManager;
 import transaction_manager.raft.sofa_jraft.RaftTransactionManagerStub;
 import transaction_manager.standalone.TransactionManagerStub;
+import transaction_manager.utils.ByteArrayWrapper;
 import utils.timer.Timer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,15 +22,15 @@ public class LoadTest {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        testParallelSingle(30000);
-        //testSequential(30000);
+        //testParallel(30000);
+        testMultiRaft(30000);
     }
 
     final static int KEY_POOL = 100;
     final static int CLIENTS = 2;
-    final static int TRANSACTIONS = 100; // sequential -> dividido pelos clients, parallel -> por client
-    final static int WRITES = 2;
-    final static int READS = 2;
+    final static int TRANSACTIONS = 1000; // sequential -> dividido pelos clients, parallel -> por client
+    final static int WRITES = 100;
+    final static int READS = 100;
 
 
     static void read(Transaction tx, Random rnd) {
@@ -174,7 +175,7 @@ public class LoadTest {
         }
     }
 
-    static void testSequential(int serverPort) throws ExecutionException, InterruptedException {
+    static void testSequentialStandaloneMultiClient(int serverPort) throws ExecutionException, InterruptedException {
 
         Timer timer = new Timer();
         timer.start();
@@ -233,6 +234,130 @@ public class LoadTest {
         timer.print();
     }
 
+    static void testMultiBD(int serverPort) throws ExecutionException, InterruptedException {
+
+        String databaseURI = "mongodb://127.0.0.1:27017";
+        String databaseName =  "testeLei";
+        String databaseCollectionName = "teste1";
+        KeyValueDriver db= new MongoAsynchKV(databaseURI, databaseName, databaseCollectionName);
+
+        Timer timer = new Timer();
+        timer.start();
+        timer.addCheckpoint("Start");
+
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+
+        Random rnd = new Random();
+        for (int i = 0; i < TRANSACTIONS; i++) {
+            pool.execute(() -> {
 
 
+
+                for (int j = 0; j < READS; j++) {
+                    String key = String.valueOf(rnd.nextInt(KEY_POOL));
+                    db.get(new ByteArrayWrapper(key.getBytes()));
+                }
+
+                Map<ByteArrayWrapper, byte[]> writeMap;
+                if (WRITES > 0) {
+                    writeMap = new HashMap<>();
+                    for (int j = 0; j < WRITES; j++) {
+                        String key = String.valueOf(rnd.nextInt(KEY_POOL));
+                        String value = String.valueOf(rnd.nextInt(KEY_POOL));
+                        writeMap.put(new ByteArrayWrapper(key.getBytes()), value.getBytes());
+                    }
+                    db.put(writeMap);
+                }
+
+            });
+        }
+
+        pool.shutdown();
+        pool.awaitTermination(60, TimeUnit.SECONDS);
+        timer.addCheckpoint("End");
+        timer.print();
+    }
+
+    static void testMultiStandalone(int serverPort) throws ExecutionException, InterruptedException {
+
+        Timer timer = new Timer();
+        timer.start();
+        timer.addCheckpoint("Start");
+
+
+        TransactionManager tms = new TransactionManagerStub(50000, serverPort);
+        TransactionController tc = new TransactionController(Address.from(60000), tms);
+        tc.buildContext();
+
+        System.out.println("Controllers ready");
+        timer.addCheckpoint("Controllers created with context");
+
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+
+        Random rnd = new Random();
+        for (int i = 0; i < TRANSACTIONS; i++) {
+            pool.execute(() -> {
+                try {
+                    Transaction tx = tc.startTransaction();
+                    for (int j = 0; j < READS; j++) {
+                        read(tx,rnd);
+                    }
+
+                    if (WRITES > 0) {
+                        for (int j = 0; j < WRITES; j++) {
+                            write(tx,rnd);
+                        }
+                    }
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        pool.shutdown();
+        pool.awaitTermination(60, TimeUnit.SECONDS);
+        timer.addCheckpoint("End");
+        timer.print();
+    }
+
+    static void testMultiRaft(int serverPort) throws ExecutionException, InterruptedException {
+
+        Timer timer = new Timer();
+        timer.start();
+        timer.addCheckpoint("Start");
+
+        TransactionManager tms = new RaftTransactionManagerStub( "manager", "127.0.0.1:8081,127.0.0.1:8082");;
+        TransactionController tc = new TransactionController(Address.from(60000), tms);
+        tc.buildContext();
+
+        System.out.println("Controllers ready");
+        timer.addCheckpoint("Controllers created with context");
+
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+
+        Random rnd = new Random();
+        for (int i = 0; i < TRANSACTIONS; i++) {
+            pool.execute(() -> {
+                try {
+                    Transaction tx = tc.startTransaction();
+                    for (int j = 0; j < READS; j++) {
+                        read(tx,rnd);
+                    }
+
+                    if (WRITES > 0) {
+                        for (int j = 0; j < WRITES; j++) {
+                            write(tx,rnd);
+                        }
+                    }
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        pool.shutdown();
+        pool.awaitTermination(60, TimeUnit.SECONDS);
+        timer.addCheckpoint("End");
+        timer.print();
+    }
 }
